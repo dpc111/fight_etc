2d5493329e82144e7476bd54b349c9c9c0397bb5
dpc111 <1539309743@qq.com>
2018-12-27 23:57:20 +0800

    l

--- config/lua/tower.lua
+++ config/lua/tower.lua
@@ -1,5 +1,6 @@
 local cfg_tower = {
 [1001] = {Id=1001,Hp=100,AttackCd=3,AttackRange=0,AttackDamage=10,AttackNum=1,BlockRange=2,Prefab=[[Elongata]],SkillId=4001,CreateCd=4,},
 [1002] = {Id=1002,Hp=100,AttackCd=4,AttackRange=0,AttackDamage=10,AttackNum=0,BlockRange=2,Prefab=[[plant]],SkillId=4003,CreateCd=3,},
+[1003] = {Id=1003,Hp=100,AttackCd=6,AttackRange=0,AttackDamage=20,AttackNum=0,BlockRange=5,Prefab=[[minigolem]],SkillId=4004,CreateCd=3,},
 }
 return cfg_tower
